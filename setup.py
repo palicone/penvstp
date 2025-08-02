95afec0af9efe3aa31edcc00b6246775bada852c
palicone <primoz@palic.si>
2025-08-02 15:43:07 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="penvstp",
+    version="0.1.0",
+    description="Environment setup automation script",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'penvstp=penvstp.cli:main',
+        ],
+    }
+)
