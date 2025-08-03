c9e0f389848f4e52654ebe6acf6fc20ad2bf9060
palicone <primoz@palic.si>
2025-08-03 11:56:00 +0200

    Added pydantic dependency for the package install

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.0",
     description="Environment setup automation script",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=["pydantic"],
     entry_points={
         'console_scripts': [
             'penvstp=penvstp.cli:main',
